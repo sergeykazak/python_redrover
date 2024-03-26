1208f33d3cbacc281d4c1decf3115bcddbdbee36
sergeykazak <sergey.kazakov@gmail.com>
2024-03-26 22:51:42 +0200

    added first selenium test file

---
+++ test_selenium1.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+browser = webdriver.Chrome()
+
+def test_auth_positive():
+    browser.get('https://www.saucedemo.com/v1/')
+    browser.find_element('xpath','//*[@id="user-name"]').send_keys('standard_user')
+    browser.find_element(By.xpath, '//*[@id="password"]').send_keys('secret_sauce')
+    browser.find_element(By.xpath, '//*[@id="login-button"]').click()
+    assert browser.current_url == 'https://www.saaaaucedemo.com/v1/inventory.html', 'Actual Result differs from Expected Result'
+    browser.quit()
